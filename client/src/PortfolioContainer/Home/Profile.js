54133b3cc7e4faeb67ef34634efc5582a67a747c
Cristian <cristian.christensen@gmail.com>
2022-04-21 11:48:05 +0200

    created folder structure and Profile component

---
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+        <div className="profile-parent">
+            <div className="profile-details">
+                <div className="colz">
+                    <a href="#">
+                        <i class="fa fa-linkedin-square"></i>
+                    </a>
+                    <a href="#">
+                        <i class="fa fa-github-square"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
